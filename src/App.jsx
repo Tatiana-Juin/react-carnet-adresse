6cd54ceadc1cc176a5ab8390afd9c1ac84afec4f
Tatiana-Juin <tatiana.juinp15@gmail.com>
2024-03-25 20:11:59 +0100

    début de la gestion du formulaire en react pour l'instant j'ai créer que les useState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import reactLogo from './assets/react.svg'
 import Cards from './components/Cards'
+
+import { useState } from 'react'
 function App() {
   
+  /* 
+    ETAPE 
+    useState => dataName (string)
+    useState => dataTel (string)
+    useState => tableau vide 
+    
+    fonction pour récupérer dataName 
+    fonction pour récupérer dataTel
+    fonction pour ajouter au tableau
 
+  */
+    // POUR LE NAME 
+    const [dataName,setDataName] = useState('');
+    // POUR LE TEL 
+    const [dataTel,setDataTel] = useState('');
+    // TABLEAU OU ON VA AJOUTER LES ELEMENTS 
+    const [contacts,setContacts] = useState([]);
+
+    
   return (
     <>
       <h1> Carnet d'adresse</h1>
@@ -18,7 +38,7 @@ function App() {
           {/* BTN */}
           <input type="submit" value="ajouter" id='btn' />
       </form>
-      <Cards />
+      
     </>
   )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Cards() {
   return (
-    <>
-        {/* <p>card</p> */}
-    </>
+    <div className='card'>
+        
+    </div>
   )
 }
